a3f83b154677b553c459bab0cbb18c0979defc5f
KERS <47688224+PluginsKers@users.noreply.github.com>
2021-01-03 14:34:40 +0800

    Update api.php
    Simplify

--- api.php
+++ api.php
@@ -1,5 +1,4 @@
 <?php
-require_once('common.php');
 header("Access-Control-Allow-Origin: *");
 header('Content-type: application/json');
 error_reporting(0);
@@ -25,17 +24,6 @@ if (!empty($_REQUEST['ip']) && !empty($_REQUEST['port'])) {
                 'gamemode' => $data['8'],
                 'delay' => round($t2 - $t1, 3) * 1000
             ];
-            $host = gethostbyname($_REQUEST['ip']);
-        	if (!mysqli_num_rows($DB->query("SELECT * FROM `list` WHERE `host` = '{$host}' and `port` = '{$_REQUEST['port']}'"))) {
-        		$DB->query("INSERT INTO `list` (`id`, `host`, `ip`, `port`, `date`) VALUES (NULL, '{$host}', '{$_REQUEST['ip']}', '{$_REQUEST['port']}', CURRENT_TIMESTAMP)");
-        	} else {
-        		if (!mysqli_num_rows($DB->query("SELECT * FROM `list` WHERE `host` = '{$host}' and `ip` = '{$_REQUEST['ip']}' and `port` = '{$_REQUEST['port']}'"))) {
-	        		$DB->query("UPDATE `list` SET `ip`='{$_REQUEST['ip']}' WHERE `host`='{$host}' and `port` = '{$_REQUEST['port']}' LIMIT 1");
-	        	} else {
-					
-	        	}
-        	}
-            //$DB->query("INSERT INTO `query` (`id`, `ip`, `port`, `date`) VALUES (NULL, '{$_REQUEST['ip']}', '{$_REQUEST['port']}', CURRENT_TIMESTAMP)");
         } else {
             $array = [
                 'status' => 'offline'
